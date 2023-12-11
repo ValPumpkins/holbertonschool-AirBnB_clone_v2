4d569bcc64abd62b1f7c30f2a64e3fa79df66b8d
ValentinePumpkins <quignon.val@gmail.com>
2023-12-11 15:58:38 +0100

    close method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,10 +55,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -79,3 +79,9 @@ class FileStorage:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    # Close
+    # ============================================================ #
+    def close(self):
+        """Calls reload() method for deserializing the JSON file to objects"""
+        self.reload()
